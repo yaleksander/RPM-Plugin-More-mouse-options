be705e449ff2004ac9525a5a498221de5f9ac282
Russo <aleksander@ice.ufjf.br>
2024-02-29 23:48:38 -0300

    minor changes

--- code.js
+++ code.js
@@ -4,8 +4,6 @@ import { THREE } from "../../System/Globals.js";
 const pluginName = "Raycaster";
 const inject = RPM.Manager.Plugins.inject;
 
-const raycaster = new THREE.Raycaster();
-
 const onClickID  = RPM.Manager.Plugins.getParameter(pluginName, "Single click event ID");
 const onDClickID = RPM.Manager.Plugins.getParameter(pluginName, "Double click event ID");
 const onDownID   = RPM.Manager.Plugins.getParameter(pluginName, "Mouse down event ID");
@@ -13,104 +11,101 @@ const onUpID     = RPM.Manager.Plugins.getParameter(pluginName, "Mouse up event 
 const onMoveID   = RPM.Manager.Plugins.getParameter(pluginName, "Mouse move event ID");
 const onWheelID  = RPM.Manager.Plugins.getParameter(pluginName, "Mouse wheel event ID");
 
-window.addEventListener("click", onClick);
-function onClick(e)
+const raycaster = new THREE.Raycaster();
+
+document.addEventListener("click", (e) =>
 {
-	if (!!RPM.Scene.Map.current && !RPM.Scene.Map.current.loading)
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
 	{
 		const x = RPM.System.DynamicValue.createNumber(e.clientX);
 		const y = RPM.System.DynamicValue.createNumber(e.clientY);
 		const b = RPM.System.DynamicValue.createNumber(e.button);
-		RPM.Manager.Events.sendEventDetection(null, -1, false, onClickID, [null, x, y]);
+		RPM.Manager.Events.sendEventDetection(null, -1, false, onClickID, [null, x, y, b]);
 	}
-}
+});
 
-window.addEventListener("dblclick", onDoubleClick);
-function onDoubleClick(e)
+document.addEventListener("dblclick", (e) =>
 {
-	if (!!RPM.Scene.Map.current && !RPM.Scene.Map.current.loading)
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
 	{
 		const x = RPM.System.DynamicValue.createNumber(e.clientX);
 		const y = RPM.System.DynamicValue.createNumber(e.clientY);
 		RPM.Manager.Events.sendEventDetection(null, -1, false, onDClickID, [null, x, y]);
 	}
-}
+});
 
-window.addEventListener("mousedown", onMouseDown);
-function onMouseDown(e)
+document.addEventListener("mousedown", (e) =>
 {
-	if (!!RPM.Scene.Map.current && !RPM.Scene.Map.current.loading)
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
 	{
 		const x = RPM.System.DynamicValue.createNumber(e.clientX);
 		const y = RPM.System.DynamicValue.createNumber(e.clientY);
 		const b = RPM.System.DynamicValue.createNumber(e.button);
-		RPM.Manager.Events.sendEventDetection(null, -1, false, onDownID, [null, x, y]);
+		RPM.Manager.Events.sendEventDetection(null, -1, false, onDownID, [null, x, y, b]);
 	}
-}
+});
 
-window.addEventListener("mouseup", onMouseUp);
-function onMouseUp(e)
+document.addEventListener("mouseup", (e) =>
 {
-	if (!!RPM.Scene.Map.current && !RPM.Scene.Map.current.loading)
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
 	{
 		const x = RPM.System.DynamicValue.createNumber(e.clientX);
 		const y = RPM.System.DynamicValue.createNumber(e.clientY);
 		const b = RPM.System.DynamicValue.createNumber(e.button);
-		RPM.Manager.Events.sendEventDetection(null, -1, false, onUpID, [null, x, y]);
+		RPM.Manager.Events.sendEventDetection(null, -1, false, onUpID, [null, x, y, b]);
 	}
-}
+});
 
-window.addEventListener("mousemove", onMouseMove);
-function onMouseMove(e)
+document.addEventListener("mousemove", (e) =>
 {
-	if (!!RPM.Scene.Map.current && !RPM.Scene.Map.current.loading)
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
 	{
 		const x = RPM.System.DynamicValue.createNumber(e.clientX);
 		const y = RPM.System.DynamicValue.createNumber(e.clientY);
 		RPM.Manager.Events.sendEventDetection(null, -1, false, onMoveID, [null, x, y]);
 	}
-}
+});
 
-window.addEventListener("wheel", onWheel);
-function onWheel(e)
+document.addEventListener("wheel", (e) =>
 {
-	if (!!RPM.Scene.Map.current && !RPM.Scene.Map.current.loading)
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
 	{
 		if (e.deltaY > 0) // scroll down
 			RPM.Manager.Events.sendEventDetection(null, -1, false, onWheelID, [null, RPM.System.DynamicValue.createSwitch(true)]);
 		else if (e.deltaY < 0) // scroll up
 			RPM.Manager.Events.sendEventDetection(null, -1, false, onWheelID, [null, RPM.System.DynamicValue.createSwitch(false)]);
 	}
-}
+});
 
-RPM.Manager.Plugins.registerCommand(pluginName, "Get object on cursor", (prop, x, y) =>
+RPM.Manager.Plugins.registerCommand(pluginName, "Get object on cursor", (variable, x, y) =>
 {
-	var obj = [];
-	var bbx = new THREE.Group();
-	x =  (x / window.innerWidth)  * 2 - 1;
-	y = -(y / window.innerHeight) * 2 + 1;
-	raycaster.setFromCamera(new THREE.Vector2(x, y), RPM.Scene.Map.current.camera.perspectiveCamera);
-	RPM.Core.ReactionInterpreter.currentObject.properties[prop] = -1;
-	for (var i = 1; i < RPM.Scene.Map.current.maxObjectsID + 1; i++)
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
 	{
-		var exitFor = false;
-		if (!RPM.Scene.Map.current.allObjects[i])
-			continue;
-		RPM.Core.MapObject.search(i, (result) =>
+		x =  (x / document.innerWidth)  * 2 - 1;
+		y = -(y / document.innerHeight) * 2 + 1;
+		raycaster.setFromCamera(new THREE.Vector2(x, y), RPM.Scene.Map.current.camera.perspectiveCamera);
+		RPM.Core.Game.current.variables[variable] = -1;
+		for (var i = 1; i < RPM.Scene.Map.current.maxObjectsID + 1; i++)
 		{
-			if (!!result)
+			var exitFor = false;
+			if (!RPM.Scene.Map.current.allObjects[i])
+				continue;
+			RPM.Core.MapObject.search(i, (result) =>
 			{
-				if (result.object.meshBoundingBox.length > 0)
+				if (!!result)
 				{
-					if (raycaster.ray.intersectsBox(result.object.meshBoundingBox[0].geometry.boundingBox))
+					if (result.object.meshBoundingBox.length > 0)
 					{
-						RPM.Core.ReactionInterpreter.currentObject.properties[prop] = i;
-						exitFor = true;
+						if (raycaster.ray.intersectsBox(result.object.meshBoundingBox[0].geometry.boundingBox))
+						{
+							RPM.Core.Game.current.variables[variable] = i;
+							exitFor = true;
+						}
 					}
 				}
-			}
-		}, RPM.Core.ReactionInterpreter.currentObject);
-		if (exitFor)
-			break;
+			}, RPM.Core.ReactionInterpreter.currentObject);
+			if (exitFor)
+				break;
+		}
 	}
 });
